5a24ca15459948defad0d93e1f4df4fb812fc5f2
jefsantos <jeh.uninove@gmail.com>
2022-06-23 01:15:16 -0300

    Versao Heroku2.1 jar

--- src/main/java/com/tasks/controller/EmailController.java
+++ src/main/java/com/tasks/controller/EmailController.java
@@ -7,10 +7,12 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class EmailController {
 
