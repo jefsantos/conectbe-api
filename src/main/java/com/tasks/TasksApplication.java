9dfdb6f6b64247ea5a60119e352b545fb8471e3a
jefsantos <jeh.uninove@gmail.com>
2022-06-27 14:52:47 -0300

    Add Cors

--- src/main/java/com/tasks/TasksApplication.java
+++ src/main/java/com/tasks/TasksApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer implements CommandLineRunner{
@@ -22,5 +25,22 @@ public class TasksApplication extends SpringBootServletInitializer implements Co
 
 
 	}
+	
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+	
+	
+	
+	
+	
+	
 
 }
